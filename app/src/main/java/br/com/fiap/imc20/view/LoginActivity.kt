2cc367981d904d14580b95be5371d1cb57a6fa8d
CELSO MARCOS FURTADO <sn1006137@CFP127ALUNO.LOCAL>
2021-12-10 15:54:54 -0300

    Aula 10-12-2021 Inicio

---
+++ app/src/main/java/br/com/fiap/imc20/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package br.com.fiap.imc20.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.fiap.imc20.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/br/com/fiap/imc20/view/NovoUsuarioActivity.kt
@@ -0,0 +1,12 @@
+package br.com.fiap.imc20.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.fiap.imc20.R
+
+class NovoUsuarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_novo_usuario)
+    }
+}
